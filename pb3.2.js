3640870d5357369772ca66abf37eae899ee0581a
Iulia <iulia.strujac@techquarter.io>
2022-07-26 00:26:00 +0300

    pb3.2

---
+++ pb3.2.js
@@ -0,0 +1,25 @@
+//3
+
+// let arr = (["Alf", "Alice", "Ben"]);
+// //  required for of
+// for (let element of arr) {
+//     console.log(element.toString())
+// }
+ 
+// // required function
+// let myFunction = (element) => element.charAt(0);
+
+//  myFunction(arr)
+ 
+
+// // required charAt
+// var str = new String( "This is string" );
+
+        
+//          console.log( arr.toString().charAt(0));
+//          console.log(  arr.toString().charAt(6));
+//          console.log(  arr.toString().charAt(12));
+//          console.log(  arr.toString().charAt(17));
+//          console.log(  arr.toString().charAt(20));
+//          console.log( arr.toString().charAt(23));
+
