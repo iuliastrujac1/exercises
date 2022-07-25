95761157048e7a45263ecce6fe4edfd5822c5031
Iulia <iulia.strujac@techquarter.io>
2022-07-26 00:25:07 +0300

    pb3

---
+++ pb3.js
@@ -0,0 +1,15 @@
+//3 
+// class customList{
+//   insertFirst(data){
+//     this.data=data;
+//   }
+//   insertLast(data){
+//     this.data=data;
+//   }
+//   getFirst(){
+//   }
+//   getLast(){
+//   }
+// }
+// var List=CustomList("11","12","11","12");
+//list.getFirst()
