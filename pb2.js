35b68a24f5bc267a223f88d3457d1382f030d74e
Iulia <iulia.strujac@techquarter.io>
2022-07-26 00:22:59 +0300

    pb2

---
+++ pb2.js
@@ -0,0 +1,14 @@
+//2
+// var options = {
+//   show: function() {
+//       console.log(1);
+//   }
+// }
+
+// var options2 = {
+//   show: function() {
+//       console.log(3);
+//   }
+// }
+// console.log(options.show)
+// //options = $.merge(options, options2);
